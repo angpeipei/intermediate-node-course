e9789f9b3f93a7c949b63e3b8b4396f8ec56025c
Peipei <peipei.ang@gmail.com>
2021-05-30 01:18:37 +0800

    Delete user

--- server.js
+++ server.js
@@ -80,15 +80,15 @@ app.route('/users/:id')
     }
   )
 })
-// // DELETE
-// .delete((req,res)=>{
-//    User.findByIdAndDelete(req.params.id, (err)=>{
-//     if (err) {
-//       res.json({success: false, message: err})
-//     } else if (!data){
-//       res.json({success: false, message: "Not Found"})
-//     } else {
-//       res.json({success: true, message:"Deleted"})
-//     }
-//    })
-// })
+// DELETE
+.delete((req,res)=>{
+   User.findByIdAndDelete(req.params.id, (err, data)=>{
+    if (err) {
+      res.json({success: false, message: err})
+    } else if (!data){
+      res.json({success: false, message: "Not Found"})
+    } else {
+      res.json({success: true, data: data})
+    }
+   })
+})
